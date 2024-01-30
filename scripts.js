93ed954bacd5139849a525d7b263186db60805e9
amvalle5 <agirl389@gmail.com>
2024-01-30 14:42:42 -0700

    Update scripts.js
    added functions

--- scripts.js
+++ scripts.js
@@ -12,12 +12,22 @@ function addTwoNums(num1, num2) {
 
 // For box 2, let's accept two string parameters and return the result of them concatenated into a single string with a space between the two words
 // name this function concatenateStrings
-// TO DO
+function concatenateStrings(str1, str2){
+    return str1 + " " + str2;
+}
+
+// concatenateStrings("Araceli", "Valles");
 
 
-// for the first function expression, let's write code that displays returns one result if the number entered by the user is less than 100 ("number is less than 100"), and another message if the number is greater than or equal to 100 ("that is 100 or greater")
+// for the first function expression, let's write code that returns one result if the number entered by the user is less than 100 ("number is less than 100"), and another message if the number is greater than or equal to 100 ("that is 100 or greater")
 // name this variable compareToHundred
-// TO DO
+let compareToHundred = function(number) {
+    if(number < 100){
+        return "Number is less than 100";
+    } else {
+        return "That is 100 or greater";
+    }
+}
 
 
 // for the second function expression, let's write code that tells the user whether or not their answer to a trivia question is correct. This will accept a parameter for a letter answer, and can use either a switch statement or a series of if/else/if statements for the logic
